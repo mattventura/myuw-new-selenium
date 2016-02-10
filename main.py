352b318b1c81811256e00f82f781c324ea8fb045
Matt Ventura <mattvent@uw.edu>
2016-02-10 12:25:02 -0800

    Removed debug option, it is now the --debug cli flag

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from myuwTesting import myuwExpected
 from myuwTesting.myuwCards import cardDict
 from myuwTesting.myuwFunctions import isCardVisible, isVisibleFast, \
     getCardName, splitList, driverRetry
+from myuwTesting.myuwHandler import mainMyuwHandler
 
 from myuwTesting.myuwTests import mainMyuwTestCase, autoDateMyuwTestCase, \
     jsonMyuwTestCase
@@ -41,66 +42,67 @@ class sampleMyuwTestCase(mainMyuwTestCase):
     testDates['seagrad'] = ('2013-1-15', )
     usersToTest = testDates.keys()
 
-            
 
-# debug option will just load the page rather than actually running any tests
-if debug:
-    # Scratch area where you can put whatever debug code
-    d = Firefox()
-    d.maximize_window()
-    m = mainMyuwHandler(d, 'http://localhost:8081/')
+# Do nothing if loaded as a module rather than run as a script
+if __name__ == '__main__':
 
-    m.setUser('jinter')
-    m.setDate('2013-05-29')
-    m.browseLanding()
-    time.sleep(4)
-    a = m.cards
-    #
-    #e = myuwExpected.getExpectedResults('javerage', '2013-06-10')
-    #diff = myuwExpected.findDiffs(a, e)
-    h = a['GradStatusCard']
-    #e = h.originalElement
+    argv = sys.argv
+    # --single option causes it to run individual test cases and report
+    # them in json format rather than doing everything
+    if len(argv) >= 3 and argv[1] == '--single':
+        # Parse arguments
+        pairs = argv[2:]
+        testDates = {}
+        for pair in pairs:
+            user, date = pair.split(':')
+            if user not in testDates:
+                testDates[user] = []
+            testDates[user].append(date)
+        users = testDates.keys()
+        # Disable parallelization
+        testconfig.parallel = False
 
-else:
+        class singleTestCase(jsonMyuwTestCase):
+            '''Test class for --single mode'''
+            testDates = testDates
+            usersToTest = users
 
-    # Do nothing if loaded as a module rather than run as a script
-    if __name__ == '__main__':
+        # Run the test
+        # This handles the output
+        unittest.TextTestRunner().run(singleTestCase('_test_json_out'))
+        
+    elif len(argv) >= 2 and argv[1] == '--dump-dates':
+        testUsers = getTestDates()
+        for user, dates in testUsers.items():
+            print 'Test dates for user %s:' %user
+            print '    ' + ', '.join([str(date) for date in dates])
+    elif len(argv) >= 2 and argv[1] == '--debug':
 
-        argv = sys.argv
-        # --single option causes it to run individual test cases and report
-        # them in json format rather than doing everything
-        if len(argv) >= 3 and argv[1] == '--single':
-            # Parse arguments
-            pairs = argv[2:]
-            testDates = {}
-            for pair in pairs:
-                user, date = pair.split(':')
-                if user not in testDates:
-                    testDates[user] = []
-                testDates[user].append(date)
-            users = testDates.keys()
-            # Disable parallelization
-            testconfig.parallel = False
+        # Scratch area where you can put whatever debug code
+        d = Firefox()
+        d.maximize_window()
+        m = mainMyuwHandler(d, 'http://localhost:8081/')
 
-            class singleTestCase(jsonMyuwTestCase):
-                '''Test class for --single mode'''
-                testDates = testDates
-                usersToTest = users
+        m.setUser('jbothell')
+        m.setDate('2013-4-7')
+        try:
+            m.browseLanding()
+        except Exception as e:
+            print e
+        el = d.find_element_by_id('SummerRegStatusCard1')
+        print el.is_displayed()
+        print repr(el.text)
+        time.sleep(4)
+        a = m.cards
+        #
+        #e = myuwExpected.getExpectedResults('javerage', '2013-06-10')
+        #diff = myuwExpected.findDiffs(a, e)
+        #h = a['GradStatusCard']
+        #e = h.originalElement
+    else:
+        main = 'mainMyuwTestCase'
+        sample = 'sampleMyuwTestCase'
+        auto = 'autoDateMyuwTestCase'
 
-            # Run the test
-            # This handles the output
-            unittest.TextTestRunner().run(singleTestCase('_test_json_out'))
-            
-        elif len(argv) >= 2 and argv[1] == '--dump-dates':
-            testUsers = getTestDates()
-            for user, dates in testUsers.items():
-                print 'Test dates for user %s:' %user
-                print '    ' + ', '.join([str(date) for date in dates])
-
-        else:
-            main = 'mainMyuwTestCase'
-            sample = 'sampleMyuwTestCase'
-            auto = 'autoDateMyuwTestCase'
-
-            # This chooses what the default test case is
-            unittest.main(defaultTest = auto)
+        # This chooses what the default test case is
+        unittest.main(defaultTest = auto)
--- testconfig.py
+++ testconfig.py
@@ -3,20 +3,18 @@
 # Some settings
 # Enable this to do some performance profiling
 perf = False
-# Runs dummy debug code instead of real test
-debug = False
 # Run each user in parallel
 parallel = True
 # Split tests into parallel processes
 # The webserver process generally doesn't use that much, 
 # and the processes are niced anyway, so whatever you would
 # use for make -jX will probably work here. 
-parallelNum = 9
+parallelNum = 8
 # Number of concurrent tests running at any given time will be at most
 # the number of users to test times parallelDateSplit
 
 # Delay between starting processes for parallel mode
-parallelDelay = 4
+parallelDelay = 5
 
 # For auto date tests, restrict dates to this range
 defaultStartDate = '2013-1-1'
